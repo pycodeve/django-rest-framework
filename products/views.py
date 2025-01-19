e7b0f2c60645faf1b115fd71770bc8f3d6b1f518
Jorge Gaitan <pycodeve@gmail.com>
2025-01-18 23:49:39 -0500

    Chore: Generic class-based views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .serializers import CategorySerializer
+from .models import Category
 
-# Create your views here.
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+
+class ListCategoriesView(ListAPIView, CreateAPIView):
+    allowed_methods = ['GET', 'POST']
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    
+class DetailCategoriesView(RetrieveUpdateDestroyAPIView):
+    allowed_methods = ['GET', 'PUT', 'DELETE']
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
--- store/urls.py
+++ store/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('coupons.urls')),
     path('api/', include('suppliers.urls')),
+    path('api/', include('products.urls')),
 ]
