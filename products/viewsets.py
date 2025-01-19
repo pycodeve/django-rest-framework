54cbf3d58c3149bacbdeb7f7ee520fbfecfdfabe
Jorge Gaitan <pycodeve@gmail.com>
2025-01-19 00:31:08 -0500

    Chore: Handling Actions with ViewSet

--- products/viewsets.py
+++ products/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import ProductSerializer
 from .models import Product
@@ -6,3 +8,17 @@ from .models import Product
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    @action(['post'], detail=True, url_path='set-on-status')
+    def set_on_status(self, request, pk=None):
+        product = self.get_object()
+        product.is_active = True
+        product.save()
+        return Response({'status': "Product is active"})
+    
+    @action(['post'], detail=True, url_path='set-off-status')
+    def set_off_status(self, request, pk=None):
+        product = self.get_object()
+        product.is_active = False
+        product.save()
+        return Response({'status': "Product is inactive"})
