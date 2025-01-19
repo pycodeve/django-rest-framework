145f388bf51c2b2a5f7f29704a22651ab16e5a76
Jorge Gaitan <pycodeve@gmail.com>
2025-01-19 00:59:51 -0500

    chore: Error Handling and Validations

--- products/serializers.py
+++ products/serializers.py
@@ -5,6 +5,11 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+    
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Price can't be negative")
+        return value
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
