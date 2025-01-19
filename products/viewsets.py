8b33e937bc9f39e0259f25b4b314b7d3d1d0cae8
Jorge Gaitan <pycodeve@gmail.com>
2025-01-19 00:53:39 -0500

    Chore: Authentication and Role Management

--- products/viewsets.py
+++ products/viewsets.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import ProductSerializer
 from .models import Product
+from .permissions import IsCustomer
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly, IsCustomer]
 
     @action(['post'], detail=True, url_path='set-on-status')
     def set_on_status(self, request, pk=None):
--- store/urls.py
+++ store/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls')),
     path('', include('docs.urls')),
     path('api/', include('coupons.urls')),
     path('api/', include('suppliers.urls')),
